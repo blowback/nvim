e5452160f11a1998c2afc9ebd60833a68b3916a9
Ant Skelton <ant@ant.org>
2024-01-02 11:35:55 +0000

    fixup comment plugin config

--- lua/core/plugins/comment.lua
+++ lua/core/plugins/comment.lua
@@ -1,5 +1,8 @@
 -- "gc" to comment lines / visual regions
-local M = { "numToStr/Comment.nvim" }
-require("Comment").setup({})
+local M = {
+  "numToStr/Comment.nvim",
+  opts = {},
+  lazy = false,
+}
 
 return M
