7c9cf19a70d1906f4aaa985eaf181c52094b6289
Ant Skelton <ant@ant.org>
2024-07-03 15:49:33 +0100

    sort out hateful prettier 2 tabspace formatting

--- lua/core/plugins/conform.lua
+++ lua/core/plugins/conform.lua
@@ -1,42 +1,49 @@
+-- conform is a formatter plugin.
+-- eg prettier, prettierd, stylua, isort, black, rstfmt
 local M = {
-    "stevearc/conform.nvim",
-    event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
-    keys = {
-        {
-            -- Customize or remove this keymap to your liking
-            "<leader>f",
-            function()
-                require("conform").format({ async = true, lsp_fallback = true })
-            end,
-            mode = "",
-            desc = "Format buffer",
-        },
-    },
-    -- Everything in opts will be passed to setup()
-    opts = {
-        -- Define your formatters
-        formatters_by_ft = {
-            lua = { "stylua" },
-            python = { "isort", "black" },
-            javascript = { { "prettierd", "prettier" } },
-        },
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			-- Customize or remove this keymap to your liking
+			"<leader>f",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = "",
+			desc = "Format buffer",
+		},
+	},
+	-- Everything in opts will be passed to setup()
+	opts = {
+		-- Define your formatters
+		formatters_by_ft = {
+			lua = { "stylua" },
+			-- run isort and black sequentially
+			python = { "isort", "black" },
+			-- run only the first available of prettierd, prettier
+			javascript = { { "prettierd", "prettier" } },
+		},
 
-        -- Set up format-on-save
-        format_on_save = { timeout_ms = 500, lsp_fallback = true },
+		-- Set up format-on-save
+		format_on_save = { timeout_ms = 500, lsp_fallback = true },
 
-        -- Customize formatters
-        formatters = {
-            shfmt = {
-                prepend_args = { "-i", "2" },
-            },
-        },
-    },
-    init = function()
-        -- If you want the formatexpr, here is the place to set it
-        vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-    end,
+		-- Customize formatters
+		formatters = {
+			shfmt = {
+				prepend_args = { "-i", "2" },
+			},
+			prettier = {
+				prepend_args = { "--tab-width", 4 },
+			},
+			prettierd = {
+				env = {
+					PRETTIERD_DEFAULT_CONFIG = vim.fn.expand("~/.config/nvim/utils/linter-config/.prettierrc.json"),
+				},
+			},
+		},
+	},
 }
 
 return M
-
