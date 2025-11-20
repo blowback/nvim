33fd4880fcffd419bb73be075d75e290cbbe3cc9
Ant Skelton <ant.skelton@planarific.com>
2025-11-20 08:45:14 +0000

    set wrap; restore 'gp'; make catpuccin default theme

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- select last pasted region
+map("", "gp", function()
+  local visual = vim.fn.getregtype()
+  local cmd = ":normal `[" .. visual .. "`]"
+  vim.api.nvim_exec2(cmd, { output = false })
+end)
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,18 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- remap leader to be COMMA
+-- remap leader to be COMMA, local leader to be SPACE
 vim.g.mapleader = ","
 vim.g.maplocalleader = " "
+
+-- disable all irritating animations
+vim.g.snacks_animate = false
+
+local opt = vim.opt
+
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.softtabstop = 0
+opt.expandtab = true
+opt.smarttab = true
+opt.wrap = true
